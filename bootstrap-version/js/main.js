3688437b19cd079ebc45a44cf09660466144e0a2
root <contacto@ivanmoreno.me>
2017-07-20 01:11:33 +0200

    [Alpha] Added new book menu

--- bootstrap-version/js/main.js
+++ bootstrap-version/js/main.js
@@ -1,11 +1,13 @@
 $(function () {
-  /* $('#logo').on('hover', function () {
-    $(this).fadeTo(50, 700)
-  }) */
+  const bookMenu = '<ul id="bookMenu"><li class="hr"><a href="#">Reading</a></li><li class="hr"><a href="#">Want to Read</a></li><li class="hr"><a href="#">Read</a></li><li style="text-align: center;"><a href="#">Delete</a></li></ul>'
 
   $('.books-grid > li').mouseenter(function () {
-    $(this).stop().fadeTo(400, 0.2)
+    $(this).find('.book-cover').stop().prepend(bookMenu)
+    $('#bookMenu').fadeIn(700)
   }).mouseleave(function () {
-    $(this).stop().fadeTo(400, 1)
+    $(this).find('.book-cover > ul').stop().fadeOut(700, () => {
+      $(this).find('.book-cover > ul').remove()
+    })
   })
+
 })
